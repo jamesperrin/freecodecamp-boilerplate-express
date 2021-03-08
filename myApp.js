84ed9ab1378e971962bd43222dbfff4b26d3418e
James Perrin <countrydawgg@gmail.com>
2021-03-08 20:34:04 +0000

    Completed section Use body-parser to Parse POST Requests

--- myApp.js
+++ myApp.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const absolutPath = __dirname;
 app.use(express.static(`${absolutPath}`));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  console.log(`${req.method} ${req.path} - ${req.ip}`);  
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
 
@@ -15,25 +17,25 @@ app.get('/', (req, res) => {
 
 app.get('/json', (req, res) => {
   const message = "Hello json";
-  const message_send = process.env.MESSAGE_STYLE === 'uppercase' ? message.toUpperCase(): message ;
-  res.json({"message": message_send});
+  const message_send = process.env.MESSAGE_STYLE === 'uppercase' ? message.toUpperCase() : message;
+  res.json({ "message": message_send });
 });
 
-app.get('/now', (req, res, next) =>{
- req.time = new Date().toString();
- next();
-}, (req, res)=>{
-  res.json({"time": req.time});
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.json({ "time": req.time });
 });
 
 app.get('/:word/echo', (req, res) => {
   const echoWord = req.params.word;
-  res.json({"echo": `${echoWord}`});
+  res.json({ "echo": `${echoWord}` });
 });
 
 app.get('/name', (req, res) => {
-  const {first, last} = req.query;
-  res.json({"name": `${first} ${last}`});
+  const { first, last } = req.query;
+  res.json({ "name": `${first} ${last}` });
 });
 
- module.exports = app;
+module.exports = app;
